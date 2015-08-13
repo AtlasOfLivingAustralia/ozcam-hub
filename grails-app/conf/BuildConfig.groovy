ba2535a86c7ba0845e52e226065bb9ae98db0f22
nickdos <nickdos@gmail.com>
2015-08-13 16:14:08 +1000

    App, Grails and biocache-hubs version updates

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -35,21 +35,10 @@ grails.project.dependency.resolution = {
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
-
-        grailsPlugins()
-        grailsHome()
         mavenLocal()
-        grailsCentral()
-        mavenCentral()
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-        mavenRepo ("http://maven.ala.org.au/repository") {
+        mavenRepo ("http://nexus.ala.org.au/content/groups/public/") {
             updatePolicy 'always'
         }
-        mavenRepo "http://maven.tmatesoft.com/content/repositories/releases/"
     }
 
     dependencies {
@@ -60,25 +49,13 @@ grails.project.dependency.resolution = {
 
     plugins {
         build ":release:3.0.1"
-        // plugins for the build system only
-        build ":tomcat:7.0.50"
-
-        // plugins for the compile step
-        //compile ":scaffolding:2.0.1"
+        build ":tomcat:7.0.54"
         compile ':cache:1.1.1'
         compile ":cache-headers:1.1.6"
-        // plugins needed at runtime but not for compilation
-        //runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.6"
-        //runtime ":database-migration:1.3.8"
-        //runtime ":jquery:1.10.2.2"
-        runtime ":resources:1.2.1"
+        //runtime ":jquery:1.11.1"
+        runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        //runtime ":zipped-resources:1.0"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
-        runtime ":biocache-hubs:[0.1,]"
-        runtime ":ala-web-theme:0.8.1"
+        runtime ":biocache-hubs:0.72-SNAPSHOT"
+        runtime ":ala-auth:1.3.1"
     }
 }
