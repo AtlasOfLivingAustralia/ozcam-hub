86bc2f4a092c4f33d9d21256f43eab4abea1a0ed
Peter Ansell <p_ansell@yahoo.com>
2016-09-20 15:12:29 +1000

    Update ala-auth-plugin to further test theory
    Test whether the current biocache-hubs and ala-auth versions are incompatible or alternatively, whether managing the versions to the most current here can fix the conflict.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -56,6 +56,6 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
         runtime ":biocache-hubs:1.1"
-        runtime ":ala-auth:1.3.1"
+        runtime ":ala-auth:1.3.4"
     }
 }
